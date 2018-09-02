afd67cf1b5cb89e411e4d2ecc0df3ccb51b78ae4
Alex Williams <alex.williams@skybettingandgaming.com>
2018-09-02 15:38:30 +0100

    put it back how it was?

--- docker-images/pptp-server/build.sh
+++ docker-images/pptp-server/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker build -t london.alexswilliams.co.uk:5000/pptp-server:latest .
-docker push london.alexswilliams.co.uk:5000/pptp-server
+docker push london.alexswilliams.co.uk:5000/pptp-server:latest
