194d0980a1c6535fd8b5a5c985c3cd164d74da6c
Alex Williams <lexw@live.co.uk>
2019-03-17 02:46:44 +0000

    Made chap secrets secret

--- docker-images/fluentd-es/build.sh
+++ docker-images/fluentd-es/build.sh
@@ -2,5 +2,7 @@
 
 set -ex
 
-docker build -t london.alexswilliams.co.uk:5000/fluentd-es:latest .
-docker push london.alexswilliams.co.uk:5000/fluentd-es:latest
+tag="$(grep 'FROM.*' Dockerfile | sed 's/FROM //' | sed 's#.*/##' | sed 's/:/-/')--$(date +%Y%m%d-%H%M)"
+
+docker build -t london.alexswilliams.co.uk:5000/fluentd-es:$tag .
+docker push london.alexswilliams.co.uk:5000/fluentd-es:$tag
--- docker-images/pptp-server/build.sh
+++ docker-images/pptp-server/build.sh
@@ -2,5 +2,7 @@
 
 set -ex
 
-docker build -t london.alexswilliams.co.uk:5000/pptp-server:latest .
-docker push london.alexswilliams.co.uk:5000/pptp-server:latest
+tag="$(grep 'FROM.*' Dockerfile | sed 's/FROM //' | sed 's#.*/##' | sed 's/:/-/')--$(date +%Y%m%d-%H%M)"
+
+docker build -t london.alexswilliams.co.uk:5000/pptp-server:$tag .
+docker push london.alexswilliams.co.uk:5000/pptp-server:$tag
