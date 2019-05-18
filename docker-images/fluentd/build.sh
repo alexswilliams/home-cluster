892ef564cc3995f499807e45050ff43df080290f
Alex Williams <lexw@live.co.uk>
2019-05-18 01:37:21 +0100

    Added label-schema.org labels to dockerfiles

--- docker-images/fluentd/build.sh
+++ docker-images/fluentd/build.sh
@@ -4,5 +4,9 @@ set -ex
 
 tag="$(grep 'FROM.*' Dockerfile | sed 's/FROM //' | sed 's#.*/##' | sed 's/:/-/')--$(date +%Y%m%d-%H%M)"
 
-docker build -t london.alexswilliams.co.uk:5000/fluentd-custom:$tag .
+docker build \
+    -t london.alexswilliams.co.uk:5000/fluentd-custom:$tag \
+    --build-arg BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ") \
+    --build-arg VCS_REF=$(git rev-parse --short HEAD) \
+    .
 docker push london.alexswilliams.co.uk:5000/fluentd-custom:$tag
